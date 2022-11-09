cf03b7415f3f790277f1ce7c228f4f1d11eddc3b
sabidalam <Shafkatsaad@gmail.com>
2022-11-09 16:46:22 +0600

    delete-data-fromdb

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
